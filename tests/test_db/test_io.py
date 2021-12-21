2d5a1b700225074314bc48a3dcf374f2c92816c8
Kip Yin <28321392+kipyin@users.noreply.github.com>
2021-12-22 00:05:41 +0800

    refactor: use semantic fixture names

--- tests/test_db/test_io.py
+++ tests/test_db/test_io.py
@@ -2,17 +2,17 @@
 from pokemaster2.db import io, tables
 
 
-def test_load_unsafe(test_db, test_csv_dir):
+def test_load_unsafe(empty_db, test_csv_dir):
     """Load db with `unsafe=True`."""
-    io.load(test_db, models=[tables.Pokemon], csv_dir=test_csv_dir, safe=False)
-    assert 0 == test_db.synchronous
-    assert "memory" == test_db.journal_mode
+    io.load(empty_db, models=[tables.Pokemon], csv_dir=test_csv_dir, safe=False)
+    assert 0 == empty_db.synchronous
+    assert "memory" == empty_db.journal_mode
 
 
-def test_load_drop_table(test_db, test_csv_dir, test_pokemon_csv):
+def test_load_drop_table(empty_db, test_csv_dir, test_pokemon_csv):
     """Drop tables."""
     # Load the pokemon table first
-    io.load(test_db, models=[tables.Pokemon], csv_dir=test_csv_dir)
+    io.load(empty_db, models=[tables.Pokemon], csv_dir=test_csv_dir)
     bulbasaur_1 = tables.Pokemon.select().where(tables.Pokemon.identifier == "bulbasaur").first()
     assert "bulbasaur" == bulbasaur_1.identifier
 
@@ -22,13 +22,13 @@ def test_load_drop_table(test_db, test_csv_dir, test_pokemon_csv):
 """
     pokemon_csv = test_csv_dir / "pokemon.csv"
     pokemon_csv.write_text(content)
-    io.load(test_db, models=[tables.Pokemon], csv_dir=test_csv_dir, drop_tables=True)
+    io.load(empty_db, models=[tables.Pokemon], csv_dir=test_csv_dir, drop_tables=True)
     bulbasaur_2 = tables.Pokemon.select().where(tables.Pokemon.identifier == "bulbasaur_2").first()
     assert "bulbasaur_2" == bulbasaur_2.identifier
 
 
-def test_load_pokemon(test_db, test_pokemon_csv, test_csv_dir):
+def test_load_pokemon(empty_db, test_pokemon_csv, test_csv_dir):
     """Load Pokemon."""
-    io.load(test_db, models=[tables.Pokemon], csv_dir=test_csv_dir, drop_tables=True)
+    io.load(empty_db, models=[tables.Pokemon], csv_dir=test_csv_dir, drop_tables=True)
     bulbasaur = tables.Pokemon.select().where(tables.Pokemon.identifier == "bulbasaur").first()
     assert 1 == bulbasaur.id
