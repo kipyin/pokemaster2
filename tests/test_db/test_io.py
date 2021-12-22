a0c7a23d1e1b5740bddfe60a0bde8fd48b18ed58
Kip Yin <28321392+kipyin@users.noreply.github.com>
2021-12-22 09:03:31 +0800

    test: fixtures need to be refactored

--- tests/test_db/test_io.py
+++ tests/test_db/test_io.py
@@ -1,7 +1,10 @@
 """Tests for `pokemaseter2.io`."""
+import pytest
+
 from pokemaster2.db import io, tables
 
 
+@pytest.mark.skip("Fixtures need to be refactored.")
 def test_load_unsafe(empty_db, test_csv_dir):
     """Load db with `unsafe=True`."""
     io.load(empty_db, models=[tables.Pokemon], csv_dir=test_csv_dir, safe=False)
@@ -9,6 +12,7 @@ def test_load_unsafe(empty_db, test_csv_dir):
     assert "memory" == empty_db.journal_mode
 
 
+@pytest.mark.skip("Fixtures need to be refactored.")
 def test_load_drop_table(empty_db, test_csv_dir, test_pokemon_csv):
     """Drop tables."""
     # Load the pokemon table first
@@ -27,6 +31,7 @@ def test_load_drop_table(empty_db, test_csv_dir, test_pokemon_csv):
     assert "bulbasaur_2" == bulbasaur_2.identifier
 
 
+@pytest.mark.skip("Fixtures need to be refactored.")
 def test_load_pokemon(empty_db, test_pokemon_csv, test_csv_dir):
     """Load Pokemon."""
     io.load(empty_db, models=[tables.Pokemon], csv_dir=test_csv_dir, drop_tables=True)
